f7800bbbf04cb236e1eba4bf89461049bf5aea63
2101070-LJI <jungin5258@gmail.com>
2025-10-29 18:47:45 +0900

    Update pub.cpp

--- rclcpp/pub.cpp
+++ rclcpp/pub.cpp
@@ -3,35 +3,33 @@
 #include "std_msgs/msg/string.hpp"
 int main(int argc, char * argv[]){
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<rclcpp::Node>("node_pub1_3");
-    auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10));
-    auto mypub = node->create_publisher<geometry_msgs::msg::Twist>("/turtle1/cmd_vel", qos_profile);
+    auto node = std::make_shared<rclcpp::Node>("node_pub1_3"); // 노드 생성
+    auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10)); // Qos 설정
+    auto mypub = node->create_publisher<geometry_msgs::msg::Twist>("/turtle1/cmd_vel", qos_profile); //turtle1/cmd_vel 토픽에 전송
 
-    geometry_msgs::msg::Twist message;
-    std_msgs::msg::String str;
-    rclcpp::WallRate loop_rate(1.0);
+    geometry_msgs::msg::Twist message; // Vector3 linear, angular
+    std_msgs::msg::String str; // String data
     
     while(rclcpp::ok()){
-        message.linear.x = 0;
-        message.angular.z = 0;
+        message.linear.x = 0; // 값 초기화
+        message.angular.z = 0; // 값 초기화
         
         std::cin>>str.data;
-        if(str.data == "f"){
+        if(str.data == "f"){ // 전진
             message.linear.x = 3;
         }
-        else if(str.data == "b"){
+        else if(str.data == "b"){ // 후진
             message.linear.x = -3;
         }
-        else if(str.data == "l"){
+        else if(str.data == "l"){ // 좌회전
             message.angular.z = -3;
         }
-        else if(str.data == "r"){
+        else if(str.data == "r"){ // 우회전
             message.angular.z = 3;
         }
         RCLCPP_INFO(node->get_logger(), "Publish: %s",str.data.c_str());
         mypub->publish(message);
         //rclcpp::spin_some(node);
-        loop_rate.sleep(); //반복주파수에서 남은 시간 만큼 sleep
     }
 
     rclcpp::shutdown();
