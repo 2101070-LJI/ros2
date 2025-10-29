37fc81c302a7fe12d15f95e5f4eca26f9beb8630
2101070-LJI <jungin5258@gmail.com>
2025-10-29 19:08:10 +0900

    Update pub.cpp

--- rclcpp/1-2/pub.cpp
+++ rclcpp/1-2/pub.cpp
@@ -2,21 +2,18 @@
 #include "geometry_msgs/msg/vector3.hpp"
 int main(int argc, char * argv[]){
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<rclcpp::Node>("node_pub1_2");
-    auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10));
-    auto mypub = node->create_publisher<geometry_msgs::msg::Vector3>("topic_pub1_2", qos_profile);
+    auto node = std::make_shared<rclcpp::Node>("node_pub1_2"); // 노드 생성
+    auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10)); // Qos설정
+    auto mypub = node->create_publisher<geometry_msgs::msg::Vector3>("topic_pub1_2", qos_profile); // topic_pub1_2에 전송
 
-    geometry_msgs::msg::Vector3 message;
-    rclcpp::WallRate loop_rate(1.0);
+    geometry_msgs::msg::Vector3 message; // float64 x, y, z
 
     while(rclcpp::ok()){
-        std::cin>>message.x;
-        std::cin>>message.y;
-        std::cin>>message.z;
+        std::cin>>message.x; // 값 입력
+        std::cin>>message.y; // 값 입력
+        std::cin>>message.z; // 값 입력
         RCLCPP_INFO(node->get_logger(), "Publish: %lf %lf %lf", message.x, message.y, message.z);
-        mypub->publish(message);
-        //rclcpp::spin_some(node);
-        loop_rate.sleep(); //반복주파수에서 남은 시간 만큼 sleep
+        mypub->publish(message); // 전송
     }
 
     rclcpp::shutdown();
